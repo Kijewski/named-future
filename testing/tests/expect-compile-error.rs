a85b468bc2ae197a036de211f4b7ce321c69eb06
René Kijewski <rene.kijewski@fu-berlin.de>
2023-05-08 17:08:19 +0200

    Update CI

--- testing/tests/expect-compile-error.rs
+++ testing/tests/expect-compile-error.rs
@@ -1,7 +1,11 @@
 use trybuild::TestCases;
+use version_check::Channel;
 
 #[cfg_attr(miri, ignore)]
 #[test]
 fn expect_compile_error() {
-    TestCases::new().compile_fail("tests/expect-compile-error/*.rs");
+    // RUSTFLAGS="--cfg IGNORE_CHANNEL" cargo +nightly test
+    if cfg!(IGNORE_CHANNEL) || Channel::read().unwrap().is_stable() {
+        TestCases::new().compile_fail("tests/expect-compile-error/*.rs");
+    }
 }
